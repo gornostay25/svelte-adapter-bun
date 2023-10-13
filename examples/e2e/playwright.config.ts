02925d66eba5dd31af21fb5b47d799c303351901
Roman <13500086+romanzy313@users.noreply.github.com>
2023-10-14 00:53:16 +0200

    implemented tests

--- examples/e2e/playwright.config.ts
+++ examples/e2e/playwright.config.ts
@@ -1,12 +1,32 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+const isDevServer = process.env.TEST_SERVER || 'dev';
+
+//
+
+const command = isDevServer == 'dev' ? 'bun run dev' : 'bun run build && bun ./build/index.js';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command,
+		port: 3000,
+		stdout: 'pipe',
+		stderr: 'pipe'
+	},
+	use: {
+		baseURL: 'http://localhost:3000',
+		trace: 'on-first-retry',
+		screenshot: 'only-on-failure'
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		}
+	]
 };
 
 export default config;
--- examples/e2e/svelte.config.js
+++ examples/e2e/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-bun';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			development: false
+		})
 	}
 };
 
